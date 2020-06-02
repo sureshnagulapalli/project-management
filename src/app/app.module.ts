f0b81b7af8821c264515c082cae060df8e01c43e
Suresh Nagulapalli <sureshnagulapalli13@gmail.com>
2020-06-01 21:03:07 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavMainComponent } from './nav-main/nav-main.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectComponent } from './project/project.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMainComponent,
+    HomeComponent,
+    ProjectComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule
